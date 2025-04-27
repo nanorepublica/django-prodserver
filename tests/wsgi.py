6539aa91d80e8ec6894595f726c7d101795a825c
Andrew Miller <info@akmiller.co.uk>
2025-04-27 22:43:16 +0100

    ci: linting

--- tests/wsgi.py
+++ tests/wsgi.py
@@ -1,4 +1,5 @@
-"""WSGI config for core project.
+"""
+WSGI config for core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -9,7 +10,6 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
