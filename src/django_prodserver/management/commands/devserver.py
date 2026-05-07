7a22c506fd23adf3761eddc5dd0378e8c0353b6e
Andrew Miller <info@akmiller.co.uk>
2026-05-07 22:07:39 +0100

    Add deprecation warning to devserver

--- src/django_prodserver/management/commands/devserver.py
+++ src/django_prodserver/management/commands/devserver.py
@@ -1,7 +1,20 @@
+import warnings
+
 from django.core.management.commands.runserver import Command as RunServerCommand
+
+DEPRECATION_MESSAGE = (
+    "The `devserver` management command is deprecated and will be removed in "
+    "django-prodserver 4.0.0. Use `python manage.py server` instead."
+)
 
 
 class Command(RunServerCommand):
     """Class to override the name of 'runserver'."""
 
-    pass
+    def handle(self, *args, **kwargs):
+        """"Override this to provide the deprecation message."""
+        warnings.warn(DEPRECATION_MESSAGE, DeprecationWarning, stacklevel=2)
+        self.stderr.write(
+            self.style.WARNING(f"DeprecationWarning: {DEPRECATION_MESSAGE}")
+        )
+        super().handle(*args, **kwargs)
